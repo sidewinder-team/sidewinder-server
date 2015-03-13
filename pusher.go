db2578b5a4bbe2ac97adac5e89205bf8ac62e329
robertfmurdock <rmurdock@pillartechnology.com>
2015-03-13 10:50:12 -0400

    Removing excess cert + key logging.

--- pusher.go
+++ pusher.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
@@ -32,12 +31,6 @@ func NewAPNSCommunicator() *APNSCommunicator {
 func makeAppleNotificationServiceClient() apns.APNSClient {
 	certificate := strings.Replace(os.Getenv("APNS_CERTIFICATE"), "\\n", "\n", -1)
 	key := strings.Replace(os.Getenv("APNS_KEY"), "\\n", "\n", -1)
-
-	fmt.Printf("CERT:\n%vKEY:\n%v", certificate, key)
 	gateway := os.Getenv("PUSH_GATEWAY")
 	return apns.BareClient(gateway, certificate, key)
 }
-
-// func makeAppleNotificationServiceClient() apns.APNSClient {
-// 	return apns.NewClient("gateway.sandbox.push.apple.com:2195", "cert-test.cert", "key-test.key")
-// }
